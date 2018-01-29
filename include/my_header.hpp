62c903b2096e792e8c59876ff2e720a236364dc2
Julesdarchez <jules.darchez@gmail.com>
2018-01-29 17:04:46 +0100

    on a fini l'ex 3

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -3,11 +3,21 @@
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
+double exo1(double rayon)
+{
+	double aire;
+	aire = 3.14 * rayon * rayon;
+	return aire;
+}
 void exo2 (short a, short b)
 {
 	short y;
 		y = a < b && !a;
 }
-
-#endif // MY_HEADER_HPP
-
+double exo3(double rayon)
+{
+	double aire;
+	aire = 4.0/3.0 * 3.14 * pow (rayon, 3);
+	return aire;
+}
+#endif
--- source/main.cpp
+++ source/main.cpp
@@ -6,11 +6,14 @@
 
 int main()
 {
-	exo2(5, 10) ;
-	exo2(0, 10) ;
-	exo2(20, 10) ;
+	double r1;
+	r1 = exo1(10.0);
     // Affichage sur un terminal
     std::cout << "Hello World!" << std::endl;
-
-    return 0;
+	exo2(5, 10);
+	exo2(0, 10);
+	exo2(20, 10);
+	exo3(10.0);
+	return 0;
 }
+
